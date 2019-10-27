afa8aeadf53e144caaa7c5770dcc91dfefc9984a
bolaji <31362517+ibrolord@users.noreply.github.com>
2019-10-27 08:59:58 -0400

    Update gitlab-runner-install.sh

--- gitlab-runner-install.sh
+++ gitlab-runner-install.sh
@@ -4,11 +4,11 @@ sudo curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloa
 sudo chmod +x /usr/local/bin/gitlab-runner ;
 curl -sSL https://get.docker.com/ | sh ;
 sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash ;
-sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner && sudo gitlab-runner start ;
-ps aux | grep gitlab-runner && sleep 5 ;
+sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner sudo gitlab-runner start ;
+ps aux | grep gitlab-runner ; 
+sleep 5 ;
 gitlab-runner register #from gitlab project settings -> runners -> expand ;
 usermod -aG docker gitlab-runner ;
 #sed '/^root    ALL=(ALL:ALL) ALL.*/a gitlab-runner ALL=(ALL) NOPASSWD: ALL ' /etc/sudoers
 sed -i '/^root.*/a gitlab-runner ALL=(ALL) NOPASSWD: ALL ' /etc/sudoers ;
 sudo gitlab-runner restart
- 
